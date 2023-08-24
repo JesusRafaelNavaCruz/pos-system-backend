ca850ef62a0bf82033d54ee1f3dc96bfc5325d2f
JesusRafaelNavaCruz <jnava@erillamhc.com>
2023-08-24 01:29:16 -0600

    CRUD OF USERS

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const userController = require('../controller/userController');
+const auth = require('../middleware/auth');
 
 const api = express.Router();
 
-api.get("/", userController.getAllUsers);
+api.get("/", auth, userController.getAllUsers);
 api.post("/", userController.addUser);
+api.put("/:id", auth, userController.updateUser);
+api.get("/:id", auth, userController.findUserById);
+api.delete("/:id", auth, userController.deleteUser);
+api.post("/signin", userController.signin)
 
 module.exports = api;
